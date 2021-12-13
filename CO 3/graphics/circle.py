b20ffc13d623e8ac126fabaaba0583153fd6fedd
Rajeesh Raj <53893306+rajeesh-coderraj@users.noreply.github.com>
2021-12-13 15:33:01 +0530

    graphics

---
+++ CO 3/graphics/circle.py
@@ -0,0 +1,6 @@
+import math
+def carea (r):
+    return math.pi*r*r
+
+def circleper(r):
+    return 2*math.pi*r
---
+++ CO 3/graphics/graphics3D/cuboid.py
@@ -0,0 +1,6 @@
+def area(l,b,h):
+     surface_area = 2*(l*b+b*h+l*h)
+     return surface_area
+
+def perimeter(l,b,h):
+    return  4*(l+b+h)
